66ba2b64aec2a12864fd2f14de96ee61d7941bb6
Jorge Zaragoza <jzaragosab.33@gmail.com>
2020-10-14 18:08:50 -0700

    Cambios sobre bandas terminadas

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,19 @@
 const {io} = require('../index');
 
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Fleetwood Mac'));
+bands.addBand(new Band('Breaking Benjamin'));
+
 // Mensajes de sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('cliente desconectado');
@@ -11,5 +22,20 @@ io.on('connection', client => {
     client.on('mensaje', (payload) => {
         console.log('Mensaje:', payload);
         io.emit('mensaje', {admin: 'Nuevo mensaje'});
+    });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+    
+    client.on('add-band', (payload) => {
+        bands.addBand(new Band(payload.name)); 
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id); 
+        io.emit('active-bands', bands.getBands());
     });
 });
